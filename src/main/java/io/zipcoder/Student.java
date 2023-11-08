2292f43359886cf56167b8605a102ca9e4eaa445
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-08 11:55:47 -0500

    writing methods for Student up until testSetExamScore

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,51 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        examScores = new ArrayList<>(Arrays.asList(testScores));
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public String getExamScores(){
+        StringBuilder sb = new StringBuilder();
+        for(Double score : examScores){
+            sb.append("Exam " + (examScores.indexOf(score)+1) + " -> " + score.intValue() + "\n");
+        }
+
+        return sb.toString();
+    }
+
+    public void addExamScore(Double score){
+        this.examScores.add(score);
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,94 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void testStudentConstructor(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+
+        Assert.assertNotNull(student);
+    }
+
+    @Test
+    public void testSetFirstName(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+        String expected = "Maria";
+        student.setFirstName(expected);
+
+        Assert.assertEquals(expected, student.getFirstName());
+    }
+
+    @Test
+    public void testSetLastName(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+        String expected = "Haha";
+        student.setLastName(expected);
+
+        Assert.assertEquals(expected, student.getLastName());
+    }
+
+    @Test
+    public void testGetNumberOfExams(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+        int expected = testScores.length;
+        student.getNumberOfExamsTaken();
+
+        Assert.assertEquals(expected, student.getNumberOfExamsTaken());
+    }
+
+    @Test
+    public void testGetExamScores(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+        String expected = "Exam 1 -> 10\nExam 2 -> 12\nExam 3 -> 20\n";
+
+        Assert.assertEquals(expected, student.getExamScores());
+
+    }
+
+    @Test
+    public void testAddExamScore(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+        student.addExamScore(30.0);
+        String expected = "Exam 1 -> 10\nExam 2 -> 12\nExam 3 -> 20\nExam 4 -> 30\n";
+
+        Assert.assertEquals(expected, student.getExamScores());
+    }
+
+    @Test
+    public void testSetExamScore(){
+        String firstName = "Tam";
+        String lastName = "Doan";
+        Double[] testScores = new Double[]{10.0, 12.0, 20.0};
+
+        Student student = new Student(firstName, lastName, testScores);
+
+    }
 
 }
