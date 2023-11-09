ed6bce24996da2412a6478f9f22fd2e61b308ea6
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-09 17:41:49 -0500

    finished

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,127 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Classroom {
+    int maxNumberOfStudents;
+    Student[] students;
+    public Classroom(){
+        students = new Student[30];
+    }
+
+    public Classroom(int maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.students = new Student[maxNumberOfStudents];
+    }
+    public Classroom(int maxNumberOfStudents, Student[] students){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.students = students;
+
+    }
+
+    public int getMaxNumberOfStudents() {
+        return maxNumberOfStudents;
+    }
+
+    public void setMaxNumberOfStudents(int maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public double getAverageExamScore(){
+        double totalStudentScores = 0.0;
+        for(Student student : students){
+            totalStudentScores += student.getAverageExamScore();
+        }
+
+        return totalStudentScores / students.length;
+    }
+
+    public void addStudent(Student student){
+        Student[] studentsNew = new Student[this.students.length + 1];
+        System.arraycopy(this.students, 0, studentsNew, 0, this.students.length);
+        studentsNew[studentsNew.length -1] = student;
+
+        this.students = studentsNew;
+
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        Student[] newStudents = new Student[this.students.length];
+        int indexOfStudentToRemove = 32; //using 32 because max students is 30
+
+        for(int i = 0; i < this.students.length; i++){
+            if(this.students[i].getFirstName().equals(firstName) && this.students[i].getLastName().equals(lastName)){
+                indexOfStudentToRemove = i;
+                this.students[i] = null;
+                break;
+            }
+        }
+
+        if(indexOfStudentToRemove == 32){
+            System.out.println("Student not found.");
+        }
+        else {
+            System.arraycopy(this.students, 0, newStudents, 0, indexOfStudentToRemove+1);
+            System.arraycopy(this.students, indexOfStudentToRemove+1, newStudents, indexOfStudentToRemove, this.students.length-1-indexOfStudentToRemove);
+            for(int i = this.students.length-indexOfStudentToRemove+1; i < newStudents.length; i++){
+                newStudents[i] = null;
+            }
+        }
+
+        this.students = newStudents;
+    }
+
+    public Map<Character, ArrayList<Student>> getGradeBook(){
+        Student highestAverageScoreer = this.students[0];
+
+        // finding highest scoring student
+        for(Student student : this.students){
+            if(highestAverageScoreer.getAverageExamScore() < student.getAverageExamScore()){
+                highestAverageScoreer = student;
+            }
+        }
+
+        // calculating percentiles using highestScorer's score
+        double highestAverage = highestAverageScoreer.getAverageExamScore();
+        double fUpperLimit = (highestAverage * 11) / 100;
+        double dUpperLimit = (highestAverage * 49) / 100;
+        double cUpperLimit = (highestAverage * 70) / 100;
+        double bUpperLimit = (highestAverage * 89) / 100;
+
+        // assigning students scores based on percentiles
+        Map<Character, ArrayList<Student>> gradeBook = new HashMap<>();
+        gradeBook.put('A', new ArrayList<Student>());
+        gradeBook.put('B', new ArrayList<Student>());
+        gradeBook.put('C', new ArrayList<Student>());
+        gradeBook.put('D', new ArrayList<Student>());
+        gradeBook.put('F', new ArrayList<Student>());
+
+
+        for (Student student : this.students) {
+            if (student.getAverageExamScore() > bUpperLimit) {
+                gradeBook.get('A').add(student);
+            } else if (student.getAverageExamScore() < bUpperLimit && student.getAverageExamScore() > cUpperLimit) {
+                gradeBook.get('B').add(student);
+            } else if (student.getAverageExamScore() < cUpperLimit && student.getAverageExamScore() > dUpperLimit) {
+                gradeBook.get('C').add(student);
+            } else if (student.getAverageExamScore() < dUpperLimit && student.getAverageExamScore() > fUpperLimit) {
+                gradeBook.get('D').add(student);
+            } else {
+                gradeBook.get('F').add(student);
+            }
+        }
+
+        return gradeBook;
+
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,142 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class ClassroomTest {
+
+    @Test
+    public void testClassroomNullConstructor(){
+        Classroom classroom = new Classroom();
+
+        Assert.assertNotNull(classroom);
+    }
+
+    @Test
+    public void testClassroomConstructor2(){
+        int maxNumberOfStudents = 20;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+
+        Assert.assertEquals(maxNumberOfStudents, classroom.getMaxNumberOfStudents());
+    }
+
+    @Test
+    public void testClassroomConstructor3(){
+        int maxNumberOfStudents = 20;
+        Student[] students = new Student[maxNumberOfStudents];
+
+        Classroom classroom = new Classroom(maxNumberOfStudents, students);
+
+        Assert.assertEquals(maxNumberOfStudents, classroom.getMaxNumberOfStudents());
+        Assert.assertEquals(students, classroom.getStudents());
+    }
+
+    @Test
+    public void testGetMaxNumberOfStudents(){
+        int maxNumberOfStudents = 10;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+
+        int actual = classroom.getMaxNumberOfStudents();
+
+        Assert.assertEquals(maxNumberOfStudents, actual);
+    }
+
+    @Test
+    public void testGetStudents(){
+
+        int maxNumberOfStudents = 10;
+        Student[] students = new Student[]{new Student("Tam", "Doan", new Double[]{12.0})};
+        Classroom classroom = new Classroom(maxNumberOfStudents, students);
+
+        Student[] actual = classroom.getStudents();
+
+        Assert.assertEquals(students, actual);
+    }
+
+    @Test
+    public void testGetAverageExamScore(){
+        int maxNumberOfStudents = 20;
+        Student[] students = new Student[]{new Student("Tam", "Doan", new Double[]{12.0, 22.0, 13.0})};
+        Classroom classroom = new Classroom(maxNumberOfStudents, students);
+
+        double expected = (12.0 + 22.0 + 13.0) / 3;
+        double classAverage = classroom.getAverageExamScore();
+
+        Assert.assertEquals(expected, classAverage, 2);
+
+    }
+
+    @Test
+    public void testAddStudent(){
+        int maxNumberOfStudents = 20;
+        Student[] students = new Student[]{new Student("Tam", "Doan", new Double[]{12.0, 22.0, 13.0})};
+        Classroom classroom = new Classroom(maxNumberOfStudents, students);
+
+        Student student = new Student("Maria", "Doan", new Double[]{13.0,22.0,50.0});
+
+        classroom.addStudent(student);
+
+        Assert.assertEquals(2, classroom.getStudents().length);
+
+    }
+
+    @Test
+    public void testRemoveStudent(){
+        int maxNumberOfStudents = 20;
+        Student[] students = new Student[]{new Student("Tam", "Doan", new Double[]{12.0, 22.0, 13.0})};
+        Classroom classroom = new Classroom(maxNumberOfStudents, students);
+
+        Student student = new Student("Maria", "Doan", new Double[]{13.0,22.0,50.0});
+        Student student2 = new Student("ha", "Doan", new Double[]{13.0,22.0,50.0});
+        Student student3 = new Student("be", "Doan", new Double[]{13.0,22.0,50.0});
+        Student student4 = new Student("sa", "Doan", new Double[]{13.0,22.0,50.0});
+        classroom.addStudent(student);
+        classroom.addStudent(student2);
+        classroom.addStudent(student3);
+        classroom.addStudent(student4);
+
+
+        classroom.removeStudent("Tam", "Doan");
+
+        Assert.assertNotNull(classroom.getStudents()[0]);
+        Assert.assertNull(classroom.getStudents()[4]);
+
+    }
+
+    @Test
+    public void testGetGradeBook(){
+        int maxNumberOfStudents = 20;
+        Student[] students = new Student[]{new Student("Tam", "Doan", new Double[]{12.0, 22.0, 13.0})};
+        Classroom classroom = new Classroom(maxNumberOfStudents, students);
+
+        Student student = new Student("Maria", "Doan", new Double[]{13.0,22.0,50.0});
+        Student student2 = new Student("ha", "Doan", new Double[]{34.0,22.0,65.0});
+        Student student3 = new Student("be", "Doan", new Double[]{78.0,26.0,26.0});
+        Student student4 = new Student("sa", "Doan", new Double[]{95.0,99.0,89.0});
+        classroom.addStudent(student);
+        classroom.addStudent(student2);
+        classroom.addStudent(student3);
+        classroom.addStudent(student4);
+
+        int expectedA = 1;
+        int expectedB = 0;
+        int expectedC = 0;
+        int expectedD = 4;
+        int expectedF = 0;
+
+        Map<Character, ArrayList<Student>> grades = classroom.getGradeBook();
+
+        // assert how many are in each grade, precalculate it beforehand.
+        Assert.assertEquals(expectedA, grades.get('A').size());
+        Assert.assertEquals(expectedB, grades.get('B').size());
+        Assert.assertEquals(expectedC, grades.get('C').size());
+        Assert.assertEquals(expectedD, grades.get('D').size());
+        Assert.assertEquals(expectedF, grades.get('F').size());
+
+
+    }
+
 }
